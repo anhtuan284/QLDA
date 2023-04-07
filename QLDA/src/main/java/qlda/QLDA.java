8def5a5cb7609a3cbc097f6076e60fcd6e7bde37
anh tuan <anhtuan28043@gmail.com>
2023-04-07 20:54:23 +0700

    test commmit2

--- QLDA/src/main/java/qlda/QLDA.java
+++ QLDA/src/main/java/qlda/QLDA.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package qlda;
 
-/**
- *
- * @author Peter
- */
 public class QLDA {
 
     public static void main(String[] args) {
