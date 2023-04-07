d395f627dcdc93ff683e9f5d3eed15776f5b1288
anh tuan <anhtuan28043@gmail.com>
2023-04-07 20:45:39 +0700

    init project

---
+++ QLDA/src/main/java/qlda/QLDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package qlda;
+
+/**
+ *
+ * @author Peter
+ */
+public class QLDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
